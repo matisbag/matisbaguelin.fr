9f4e197d2618f083c8e2eb740982aa41372a5529
Matis <matis.baguelin@gmail.com>
2021-01-31 00:46:47 +0100

    commit

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="header">
     <nav>
       <div class="logo">
         <NuxtLink to="/">codewithchu</NuxtLink>
@@ -17,15 +17,18 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-nav {
-  @apply flex justify-between items-center py-2 px-10 fixed w-full bg-white shadow-lg;
-  .logo {
-    @apply p-4 font-bold;
-  }
-  .btn {
-    @apply px-4 py-2 text-gray-500 rounded-lg hover:bg-emerald-100 hover:text-emerald-700;
-    &.nuxt-link-exact-active {
-      @apply bg-emerald-100 text-emerald-700;
+.header {
+  @apply bg-white shadow-lg w-full fixed;
+  nav {
+    @apply container mx-auto flex justify-between items-center py-2 px-10;
+    .logo {
+      @apply p-4 font-bold;
+    }
+    .btn {
+      @apply px-4 py-2 text-gray-500 rounded-lg hover:bg-emerald-100 hover:text-emerald-700;
+      &.nuxt-link-exact-active {
+        @apply bg-emerald-100 text-emerald-700;
+      }
     }
   }
 }
