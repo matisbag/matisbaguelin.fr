87549b5cf7a563ce91bff8b7f96bdaaf1cc7af65
Matis <matis.baguelin@gmail.com>
2021-02-15 00:37:29 +0100

    skills component

---
+++ components/Skills.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="max-w-4xl mx-auto my-1">
+    <div class="rounded-lg bg-black shadow-md sm:grid cursor-pointer" :class="'sm:grid-cols-' + skills.skills.length">
+      <div v-for="(skill, index) in skills.skills" :key="skill.name" class="border-gray-500 group bg-gradient-to-b hover:from-black" :class="[index === 0 ? 'rounded-t-lg sm:rounded-t-none sm:rounded-l-lg sm:border-0' : 'border-t sm:border-0 sm:border-l', index === skills.skills.length - 1 ? 'rounded-b-lg sm:rounded-b-none sm:rounded-r-lg' : 'border-b sm:border-r', 'hover:to-' + skills.color]">
+        <div class="p-6 text-center">
+          <p class="text-5xl leading-none font-extrabold text-indigo-600">
+            <img :src="require(`~/assets/${skill.icon}`)" :alt="skill.name" class="w-10 h-10 mx-auto">
+          </p>
+          <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
+            {{skill.name}}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    skills: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
--- components/SkillsSection.vue
+++ components/SkillsSection.vue
@@ -3,59 +3,14 @@
     <div id="skills" class="lg:text-center">
       <h2 class="text-base text-emerald-500 font-semibold tracking-wide uppercase">Mes compétences</h2>
       <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-100 sm:text-4xl">
-        Frameworks de prédilections
+        Techno de prédilections
       </p>
       <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-        Mes années d'expériences m'ont permis de découvrir ses merveilleux frameworks 😁
+        Mes années d'expériences m'ont permis de découvrir ses merveilleuses techno 😁
       </p>
     </div>
 
-    <section class="max-w-4xl mx-auto my-1">
-      <div class="rounded-lg bg-black shadow-lg sm:grid sm:grid-cols-3">
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black hover:to-dark-green rounded-tl-lg rounded-bl-lg">
-          <div class="border-b border-gray-500 p-6 text-center sm:border-0 sm:border-r">
-            <p class="text-5xl leading-none font-extrabold text-blue-500">
-              <svg class="w-10 h-10 mx-auto" viewBox="0 0 256 221">
-                <g>
-                  <path d="M204.8,0 L256,0 L128,220.8 L0,0 L50.56,0 L97.92,0 L128,51.2 L157.44,0 L204.8,0 Z" fill="#41B883"></path>
-                  <path d="M0,0 L128,220.8 L256,0 L204.8,0 L128,132.48 L50.56,0 L0,0 Z" fill="#41B883"></path>
-                  <path d="M50.56,0 L128,133.12 L204.8,0 L157.44,0 L128,51.2 L97.92,0 L50.56,0 Z" fill="#35495E"></path>
-                </g>
-              </svg>
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Vue.js
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-dark-green">
-          <div class="border-t border-b border-gray-500 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <svg class="w-10 h-10 mx-auto" viewBox="0 0 50 52" xmlns="http://www.w3.org/2000/svg">
-                <path d="M49.626 11.564a.809.809 0 0 1 .028.209v10.972a.8.8 0 0 1-.402.694l-9.209 5.302V39.25c0 .286-.152.55-.4.694L20.42 51.01c-.044.025-.092.041-.14.058-.018.006-.035.017-.054.022a.805.805 0 0 1-.41 0c-.022-.006-.042-.018-.063-.026-.044-.016-.09-.03-.132-.054L.402 39.944A.801.801 0 0 1 0 39.25V6.334c0-.072.01-.142.028-.21.006-.023.02-.044.028-.067.015-.042.029-.085.051-.124.015-.026.037-.047.055-.071.023-.032.044-.065.071-.093.023-.023.053-.04.079-.06.029-.024.055-.05.088-.069h.001l9.61-5.533a.802.802 0 0 1 .8 0l9.61 5.533h.002c.032.02.059.045.088.068.026.02.055.038.078.06.028.029.048.062.072.094.017.024.04.045.054.071.023.04.036.082.052.124.008.023.022.044.028.068a.809.809 0 0 1 .028.209v20.559l8.008-4.611v-10.51c0-.07.01-.141.028-.208.007-.024.02-.045.028-.068.016-.042.03-.085.052-.124.015-.026.037-.047.054-.071.024-.032.044-.065.072-.093.023-.023.052-.04.078-.06.03-.024.056-.05.088-.069h.001l9.611-5.533a.801.801 0 0 1 .8 0l9.61 5.533c.034.02.06.045.09.068.025.02.054.038.077.06.028.029.048.062.072.094.018.024.04.045.054.071.023.039.036.082.052.124.009.023.022.044.028.068zm-1.574 10.718v-9.124l-3.363 1.936-4.646 2.675v9.124l8.01-4.611zm-9.61 16.505v-9.13l-4.57 2.61-13.05 7.448v9.216l17.62-10.144zM1.602 7.719v31.068L19.22 48.93v-9.214l-9.204-5.209-.003-.002-.004-.002c-.031-.018-.057-.044-.086-.066-.025-.02-.054-.036-.076-.058l-.002-.003c-.026-.025-.044-.056-.066-.084-.02-.027-.044-.05-.06-.078l-.001-.003c-.018-.03-.029-.066-.042-.1-.013-.03-.03-.058-.038-.09v-.001c-.01-.038-.012-.078-.016-.117-.004-.03-.012-.06-.012-.09v-.002-21.481L4.965 9.654 1.602 7.72zm8.81-5.994L2.405 6.334l8.005 4.609 8.006-4.61-8.006-4.608zm4.164 28.764l4.645-2.674V7.719l-3.363 1.936-4.646 2.675v20.096l3.364-1.937zM39.243 7.164l-8.006 4.609 8.006 4.609 8.005-4.61-8.005-4.608zm-.801 10.605l-4.646-2.675-3.363-1.936v9.124l4.645 2.674 3.364 1.937v-9.124zM20.02 38.33l11.743-6.704 5.87-3.35-8-4.606-9.211 5.303-8.395 4.833 7.993 4.524z" fill="#FF2D20" fill-rule="evenodd"/>
-              </svg>
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Laravel
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-dark-green rounded-tr-lg rounded-br-lg">
-          <div class="border-t border-gray-500 p-6 text-center sm:border-0 sm:border-l">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <svg class="w-10 h-10 mx-auto" viewBox="0 0 256 289" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
-                <g>
-                  <path d="M127.999999,288.463771 C124.024844,288.463771 120.314699,287.403728 116.869564,285.548656 L81.6231884,264.612838 C76.32298,261.697724 78.9730854,260.637682 80.5631458,260.107661 C87.7184259,257.72257 89.0434775,257.192547 96.4637688,252.952381 C97.2587979,252.422361 98.3188405,252.687372 99.1138718,253.217392 L126.144927,269.383024 C127.20497,269.913045 128.530021,269.913045 129.325053,269.383024 L235.064182,208.165634 C236.124225,207.635611 236.654245,206.575571 236.654245,205.250519 L236.654245,83.0807467 C236.654245,81.7556929 236.124225,80.6956526 235.064182,80.1656324 L129.325053,19.2132506 C128.26501,18.6832305 126.939959,18.6832305 126.144927,19.2132506 L20.4057954,80.1656324 C19.3457551,80.6956526 18.8157349,82.0207041 18.8157349,83.0807467 L18.8157349,205.250519 C18.8157349,206.31056 19.3457551,207.635611 20.4057954,208.165634 L49.2919247,224.861286 C64.9275364,232.811595 74.7329196,223.536234 74.7329196,214.260871 L74.7329196,93.681159 C74.7329196,92.0910985 76.0579711,90.5010358 77.9130428,90.5010358 L91.4285716,90.5010358 C93.0186343,90.5010358 94.6086948,91.8260873 94.6086948,93.681159 L94.6086948,214.260871 C94.6086948,235.196689 83.2132512,247.387164 63.3374737,247.387164 C57.2422362,247.387164 52.4720502,247.387164 38.9565214,240.761906 L11.1304347,224.861286 C4.24016581,220.886129 5.68434189e-14,213.46584 5.68434189e-14,205.515528 L5.68434189e-14,83.3457557 C5.68434189e-14,75.3954465 4.24016581,67.9751552 11.1304347,64.0000006 L116.869564,2.78260752 C123.494824,-0.927535841 132.505176,-0.927535841 139.130436,2.78260752 L244.869565,64.0000006 C251.759834,67.9751552 256,75.3954465 256,83.3457557 L256,205.515528 C256,213.46584 251.759834,220.886129 244.869565,224.861286 L139.130436,286.078676 C135.685299,287.668739 131.710145,288.463771 127.999999,288.463771 L127.999999,288.463771 Z M160.596274,204.455488 C114.219461,204.455488 104.679089,183.254659 104.679089,165.233955 C104.679089,163.643893 106.004141,162.053832 107.859212,162.053832 L121.639752,162.053832 C123.229813,162.053832 124.554864,163.113872 124.554864,164.703935 C126.674947,178.749484 132.770187,185.639753 160.861283,185.639753 C183.122154,185.639753 192.662526,180.604556 192.662526,168.67909 C192.662526,161.788821 190.012423,156.753624 155.296065,153.308489 C126.409938,150.393375 108.389235,144.033126 108.389235,120.977226 C108.389235,99.5113875 126.409938,86.7908901 156.621119,86.7908901 C190.542443,86.7908901 207.238095,98.4513472 209.358178,123.89234 C209.358178,124.687371 209.093167,125.482403 208.563147,126.277434 C208.033127,126.807454 207.238095,127.337474 206.443064,127.337474 L192.662526,127.337474 C191.337475,127.337474 190.012423,126.277434 189.747412,124.952382 C186.567289,110.376813 178.351966,105.606625 156.621119,105.606625 C132.240165,105.606625 129.325053,114.086957 129.325053,120.447205 C129.325053,128.132506 132.770187,130.5176 165.631471,134.757766 C198.227744,138.997931 213.598344,145.093169 213.598344,167.884058 C213.333333,191.20497 194.252589,204.455488 160.596274,204.455488 L160.596274,204.455488 Z" fill="#539E43"></path>
-                </g>
-              </svg>
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Node.js
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <skills :skills="predilectionsSkills"/>
 
     <p class="lg:text-center mt-8 text-3xl leading-8 font-extrabold tracking-tight text-gray-100 sm:text-4xl">
       Quelques expériences
@@ -64,148 +19,100 @@
       Technologies avec lesquelles j'ai déjà travaillé
     </p>
 
-    <section class="max-w-4xl mx-auto my-1">
-      <div class="rounded-lg bg-black shadow-lg sm:grid sm:grid-cols-3">
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black hover:to-pink-900 rounded-tl-lg rounded-bl-lg">
-          <div class="border-b border-gray-500 p-6 text-center sm:border-0 sm:border-r">
-            <p class="text-5xl leading-none font-extrabold text-blue-500">
-              <svg class="w-10 h-10 mx-auto" viewBox="0 0 256 270" xmlns="http://www.w3.org/2000/svg">
-                <path d="M127.606.341L.849 44.95 20.88 211.022l106.86 58.732 107.412-59.528L255.175 44.16 127.606.341z" fill="#B3B3B3"/>
-                <path d="M242.532 53.758L127.31 14.466v241.256l96.561-53.441 18.66-148.523z" fill="#A6120D"/>
-                <path d="M15.073 54.466l17.165 148.525 95.07 52.731V14.462L15.074 54.465z" fill="#DD1B16"/>
-                <path d="M159.027 142.898L127.31 157.73H93.881l-15.714 39.305-29.228.54L127.31 23.227l31.717 119.672zm-3.066-7.467l-28.44-56.303-23.329 55.334h23.117l28.652.97z" fill="#F2F2F2"/>
-                <path d="M127.309 23.226l.21 55.902 26.47 55.377h-26.62l-.06 23.189 36.81.035 17.204 39.852 27.967.518-81.981-174.873z" fill="#B3B3B3"/>
-              </svg>
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Angular
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-dark-green">
-          <div class="border-t border-b border-gray-500 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <svg class="w-10 h-10 mx-auto" viewBox="0 0 256 257" xmlns="http://www.w3.org/2000/svg">
-                <circle fill="#1A171B" cx="128" cy="128.827" r="128"/>
-                <path d="M183.706 48.124c-12.986.453-24.32 7.61-32.757 17.51-9.342 10.855-15.557 23.73-20.035 36.872-8.01-6.565-14.19-15.064-27.041-18.77-9.933-2.852-20.366-1.674-29.96 5.474-4.545 3.395-7.676 8.527-9.165 13.351-3.855 12.537 4.053 23.694 7.645 27.7l7.853 8.416c1.619 1.65 5.518 5.955 3.612 12.127-2.06 6.71-10.15 11.055-18.448 8.495-3.706-1.13-9.03-3.891-7.838-7.779.493-1.59 1.631-2.78 2.241-4.155.56-1.181.827-2.067.997-2.587 1.516-4.95-.555-11.39-5.857-13.025-4.946-1.516-10.007-.315-11.969 6.054-2.225 7.235 1.237 20.366 19.783 26.084 21.729 6.676 40.11-5.155 42.717-20.586 1.642-9.665-2.722-16.845-10.717-26.08l-6.514-7.204c-3.946-3.942-5.301-10.661-1.217-15.825 3.446-4.356 8.354-6.215 16.392-4.029 11.733 3.186 16.963 11.327 25.69 17.893-3.603 11.819-5.958 23.682-8.09 34.32l-1.299 7.931c-6.238 32.721-11 50.688-23.375 61.003-2.493 1.773-6.057 4.427-11.429 4.612-2.816.087-3.726-1.85-3.765-2.694-.067-1.977 1.599-2.883 2.706-3.773 1.654-.902 4.155-2.398 3.985-7.191-.18-5.664-4.872-10.575-11.654-10.35-5.08.173-12.823 4.954-12.532 13.705.303 9.039 8.728 15.813 21.43 15.384 6.79-.233 21.952-2.997 36.895-20.76 17.392-20.362 22.256-43.705 25.915-60.79l4.084-22.556c2.269.272 4.695.453 7.334.516 21.661.457 32.496-10.763 32.657-18.924.107-4.939-3.241-9.799-7.928-9.689-3.355.095-7.57 2.328-8.582 6.968-.988 4.552 6.893 8.66.733 12.65-4.376 2.832-12.221 4.828-23.269 3.206l2.009-11.103c4.1-21.055 9.157-46.954 28.341-47.584 1.398-.071 6.514.063 6.633 3.446.035 1.13-.245 1.418-1.568 4.005-1.347 2.017-1.855 3.734-1.792 5.707.185 5.376 4.273 8.909 10.185 8.696 7.916-.256 10.193-7.963 10.063-11.921-.32-9.3-10.122-15.175-23.1-14.75z" fill="#FFF"/>
-              </svg>
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Symfony
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-dark-green rounded-tr-lg rounded-br-lg">
-          <div class="border-t border-gray-500 p-6 text-center sm:border-0 sm:border-l">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <img src="~/assets/Kotlin-logo.svg" alt="" class="w-10 h-10 mx-auto">
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Kotlin
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <skills :skills="othersSkills"/>
+
+
 
     <p class="lg:text-center mt-8 text-3xl leading-8 font-extrabold tracking-tight text-gray-100 sm:text-4xl">
       Outils
     </p>
     <p class="lg:text-center mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-      Technologies avec lesquelles j'ai déjà travaillé
+      Outils que j'ai déjà utilisé
     </p>
 
-    <section class="max-w-4xl mx-auto my-1">
-      <div class="rounded-lg bg-black shadow-lg sm:grid sm:grid-cols-6">
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black hover:to-indigo-900 rounded-tl-lg rounded-bl-lg sm:border-0 sm:border-r">
-          <div class="border-b sm:border-b-0 border-gray-500 p-6 text-center">
-            <p class="text-5xl leading-none font-extrabold text-blue-500">
-              <img src="~assets/nuxtjs.svg" alt="" class="w-10 h-10 mx-auto">
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              NuxtJS
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-indigo-900 sm:border-0 sm:border-r">
-          <div class="border-t border-b border-gray-500 p-6 text-center sm:border-0">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <img src="~assets/tailwindcss.svg" alt="" class="w-10 h-10 mx-auto">
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Tailwind CSS
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-indigo-900 sm:border-0 sm:border-r">
-          <div class="border-t border-b border-gray-500 p-6 text-center sm:border-0">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <img src="~assets/vuetify.svg" alt="" class="w-10 h-10 mx-auto">
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Vuetify
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-indigo-900 sm:border-0 sm:border-r">
-          <div class="border-t border-b border-gray-500 p-6 text-center sm:border-0">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <img src="~assets/git.svg" alt="" class="w-10 h-10 mx-auto">
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Git
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-indigo-900 sm:border-0 sm:border-r">
-          <div class="border-t border-b border-gray-500 p-6 text-center sm:border-0">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <img src="~assets/gitkraken.svg" alt="" class="w-10 h-10 mx-auto">
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Git Kraken
-            </p>
-          </div>
-        </div>
-        <div class="cursor-pointer group bg-gradient-to-b hover:from-black  hover:to-indigo-900 rounded-tr-lg rounded-br-lg">
-          <div class="border-t sm:border-0 border-gray-500 p-6 text-center">
-            <p class="text-5xl leading-none font-extrabold text-indigo-600">
-              <img src="~/assets/Kotlin-logo.svg" alt="" class="w-10 h-10 mx-auto">
-            </p>
-            <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
-              Autre
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <skills :skills="toolsSkills"/>
   </section>
 </template>
 
+
 <script>
-  import Project from './Project.vue'
+
+// delete p > img style
+
+  import Skills from './Skills.vue'
   export default {
     components: {
-      Project
+      Skills
     },
     data() {
       return {
-        projects: []
+        predilectionsSkills: {
+          skills: [
+            {
+              name: 'Vue.js',
+              icon: 'vuejs.svg'
+            },
+            {
+              name: 'Laravel',
+              icon: 'laravel.svg'
+            },
+            {
+              name: 'Node.js',
+              icon: 'nodejs.svg'
+            }
+          ],
+          color: 'dark-green'
+          // else ?
+        },
+        othersSkills: {
+          skills: [
+            {
+              name: 'Angular',
+              icon: 'angular.svg',
+            },
+            {
+              name: 'Symfony',
+              icon: 'symfony.svg',
+            },
+            {
+              name: 'Kotlin',
+              icon: 'kotlin.svg',
+            }
+          ],
+          color: 'pink-900'
+          // else ?
+        },
+        toolsSkills: {
+          skills: [
+            {
+              name: 'NuxtJS',
+              icon: 'nuxtjs.svg',
+            },
+            {
+              name: 'TailwindCSS',
+              icon: 'tailwindcss.svg',
+            },
+            {
+              name: 'Vuetify',
+              icon: 'vuetify.svg',
+            },
+            {
+              name: 'Git',
+              icon: 'git.svg',
+            },
+            {
+              name: 'GitKraken',
+              icon: 'gitkraken.svg',
+            },
+            {
+              name: 'Sass',
+              icon: 'sass.svg',
+            }
+          ],
+          color: 'indigo-900'
+          // else ?
+        },
       }
     },
-    created() {
-      this.projects = this.$store.state.projects.projects
-    },
-    head() {
-      return {
-        title: 'Matis Baguelin - Compétences',
-        meta: [{
-          hid: 'description',
-          name: 'description',
-          content: 'Mes compétences'
-        }],
-      }
-    }
   }
-
 </script>
