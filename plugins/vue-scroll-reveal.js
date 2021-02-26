5d6d2484e3b87e0ab1af94dc9250ef7001a1686b
Matis <matis.baguelin@gmail.com>
2021-02-27 00:55:24 +0100

    scroll reveal

---
+++ plugins/vue-scroll-reveal.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueScrollReveal from 'vue-scroll-reveal';
+ 
+// https://www.npmjs.com/package/vue-scroll-reveal
+// Vue.use(VueScrollReveal);
+Vue.use(VueScrollReveal, {
+  duration: 800,
+  scale: 0.95,
+  distance: '10px',
+  mobile: false,
+  reset: true,
+  viewOffset: {
+    top: -10,
+  },
+});
