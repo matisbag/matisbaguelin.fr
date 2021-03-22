f4db98f807bc4beadb8283084c7aeaad1eaf23d7
Matis <matisbag77@gmail.com>
2021-03-22 23:55:15 +0100

    404 + design project

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -1,9 +1,15 @@
 <template>
-  <main>
-    <!-- <Project v-if="project" :project="project"/>
-    <div v-else>
-      <p>bonjour a tous</p>
-    </div> -->
+  <main class="flex-1 px-4 py-10">
+    <article class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div class="w-full flex flex-col items-center">
+        <h1 class="text-3xl font-bold tracking-tight text-white sm:text-4xl mb-4">{{ project.title }}</h1>
+        <img class="max-w-5xl rounded-lg" src="~/assets/conference.png">
+      </div>
+      <div class="py-6">
+        <p class="text-gray-200 text-sm lg:text-lg">{{project.description}}</p>
+      </div>
+    </article>
+    
   </main>
 </template>
 
@@ -16,6 +22,7 @@ export default {
   },
   created() {
     this.project = this.$store.state.projects.projects.filter(project => project.routeName === this.$route.params.project)[0]
+    console.log(this.project);
   },
   head() {
     return {
