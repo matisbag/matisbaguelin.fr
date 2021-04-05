d6e03add713f7b89a84a88437732714526c2160a
Matis <matis.baguelin@gmail.com>
2021-04-05 02:18:00 +0200

    fix

--- components/Project.vue
+++ components/Project.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="relative group flex flex-col rounded-lg max-w-5xl transform sm:hover:scale-105 duration-500" v-scroll-reveal>
     <nuxt-link :to="'/projects/' + project.routeName">
-      <img class="rounded-lg" :src="require(`~/assets/${project.image}`)">
+      <img class="rounded-lg" :src="require(`~/assets/${project.image}.png`)">
       <div>
         <div class="flex space-x-2 my-1">
           <span v-if="project.github" class="flex items-center px-1"><img src="~/assets/icons/github.svg" alt="icon github" class="h-6"></span>
--- components/Skills.vue
+++ components/Skills.vue
@@ -4,7 +4,7 @@
       <div v-for="(skill, index) in skills.skills" :key="skill.name" class="border-gray-500 group bg-gradient-to-b hover:from-black" :class="[index === 0 ? 'rounded-t-lg sm:rounded-t-none sm:rounded-l-lg sm:border-0' : 'border-t sm:border-0 sm:border-l', index === skills.skills.length - 1 ? 'rounded-b-lg sm:rounded-b-none sm:rounded-r-lg' : 'border-b sm:border-r', 'hover:to-' + skills.color]">
         <div class="py-4 sm:py-6 px-1 text-center">
           <p class="text-5xl leading-none font-extrabold text-indigo-600">
-            <img :src="require(`~/assets/${skill.icon}`)" :alt="skill.name" class="w-10 h-10 mx-auto">
+            <img :src="require(`~/assets/${skill.icon}.svg`)" :alt="skill.name" class="w-10 h-10 mx-auto">
           </p>
           <p class="mt-2 text-lg leading-6 font-medium text-cool-gray-500">
             {{skill.name}}
