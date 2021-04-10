e18dd0a6a9a87be04aaad39b6cb667c4dacc45f2
Matis <matisbag77@gmail.com>
2021-04-10 22:32:23 +0200

    fix

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="flex-1 px-4 py-10">
-    <article class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+    <article class="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
       <div class="lg:text-center">
         <h2 class="text-base text-green-500 font-semibold tracking-wide uppercase">{{ project.title }}</h2>
         <p class="my-2 text-3xl leading-8 font-bold tracking-tight text-gray-100 sm:text-4xl">
@@ -11,9 +11,16 @@
         <img class="max-w-5xl w-full rounded-lg" :src="require(`~/assets/${project.image}.png`)">
       </div>
       <div class="py-6">
-        <a :href="project.github">Github</a>
-        <a :href="project.link">Voir le projet</a>
-        <p class="text-white lg:text-lg font-medium text-justify" v-html="project.description"></p>
+        <div class="flex justify-center space-x-2">
+          <a :href="project.github" target="_blank" class="bg-black font-bold tracking-wide px-4 py-2 rounded-md hover:rounded-xl transition duration-1000 ease-in-out flex items-center space-x-2">
+            <img src="~/assets/icons/github.svg" alt="github icon" class="h-6 w-6">
+            <span>Github</span>
+          </a>
+          <a :href="project.link" target="_blank" class="bg-gray-800 font-bold tracking-wide px-4 py-2 rounded-md hover:rounded-xl transition duration-1000 ease-in-out">Visiter le site</a>
+        </div>
+        <div class="py-6">
+          <p class="text-white lg:text-lg font-medium text-justify" v-html="project.description"></p>
+        </div>
       </div>
     </article>
   </main>
@@ -33,7 +40,7 @@ export default {
         {
           hid: 'description',
           name: 'description',
-          content: this.project.description
+          content: this.project.desc
         }
       ]
     }
