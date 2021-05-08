03d09d642daab2ee537cb532019f33d5ba3a29b6
Matis <matis.baguelin@gmail.com>
2021-05-09 01:41:04 +0200

    tailwind jit

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,5 +57,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
+  },
+
+  // https://tailwindcss.nuxtjs.org/setup/
+  tailwindcss: {
+    jit: true
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,33 @@
 const colors = require('tailwindcss/colors');
+
 module.exports = {
+  mode: 'jit',
   purge: {
     content: [
+      './safelist.txt',
       './components/**/*.{vue,js}',
       './layouts/**/*.vue',
       './pages/**/*.vue',
       './plugins/**/*.{js,ts}',
       './nuxt.config.{js,ts}',
     ],
-    options: {
-      safelist: [/^sm:grid-cols-/],
-    }
+    // options: {
+    //   safelist: [/^sm:grid-cols-/],
+    // }
 },
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         gray: colors.trueGray,
+        green: colors.emerald,
         'dark-green': '#011c15',
         'dark-pink' : '#260713',
         'dark-blue' : '#0f0d26'
       },
     },
   },
-  variants: {
-    extend: {
-      margin: ['group-hover'],
-      opacity: ['group-hover'],
-      scale: ['group-hover'],
-      borderRadius: ['hover'],
-      display: ['dark']
-    },
-  },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+  ],
 }
