7af8229e12717bf60824c7d86fb958304c7757d0
Matis <matis.baguelin@gmail.com>
2022-01-13 15:02:57 +0100

    fix hot reload

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,7 +65,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxt/postcss8'
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
