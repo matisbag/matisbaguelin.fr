2c62328dff183469dfb226c96ec6d7f4176db541
Matis <matis.baguelin@gmail.com>
2024-03-04 17:01:17 +0100

    init nuxt 3

--- layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="flex flex-col min-h-screen">
-    <AppBar />
-    <Nuxt keep-alive :keep-alive-props="{ max: 5 }" class="flex-1 px-4 py-10" />
-    <Footer />
-  </div>
-</template>
