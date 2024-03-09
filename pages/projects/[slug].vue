693cab0e93d198892b77fb457a79a3ccd4a1e514
Matis <matis.baguelin@gmail.com>
2024-03-09 18:22:56 +0100

    feat: configure projects with @nuxt/content

---
+++ pages/projects/[slug].vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <ContentDoc :path="slug" v-slot="{ doc }">
+      <article>
+        <h1>{{ doc.title }}</h1>
+        <ContentRenderer :value="doc" />
+      </article>
+    </ContentDoc>
+  </main>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+const { slug } = route.params
+</script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <main>
+    <ContentList path="/" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <a :href="`/projects${article._path}`">
+          <h2>{{ article.title }}</h2>
+        </a>
+      </div>
+    </ContentList>
+  </main>
 </template>
 
 <script lang="ts" setup></script>
-
-<style></style>
