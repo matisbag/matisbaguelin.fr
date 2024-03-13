628610eff5b9228b43066ffffa3481dba598b6a6
Matis <matis.baguelin@gmail.com>
2024-03-13 14:03:19 +0100

    feat(ProjectCard): update project type

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,16 +1,18 @@
 <template>
   <main>
-    <ContentList path="/" v-slot="{ list }">
-      <ProjectCard
-        v-for="project in list"
-        :item="project"
-        class="py-6"
-        :class="{ 'border-t-[1px] border-gray-200': project !== list[0] }"
-      />
-    </ContentList>
+    <ProjectCard
+      v-for="(project, index) in projects"
+      :item="project"
+      class="py-6"
+      :class="{ 'border-t-[1px] border-gray-200': index !== 0 }"
+    />
   </main>
 </template>
 
 <script lang="ts" setup>
 import ProjectCard from "~/components/ProjectCard.vue"
+
+const { data: projects } = await useAsyncData("projects", () =>
+  queryContent().find(),
+)
 </script>
