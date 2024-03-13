41394ac099a72e3daf882c44f9749607455541fe
Matis <mbaguelin@expertises-galtier.fr>
2024-03-13 16:16:20 +0100

    feat: add @nuxt/image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "@nuxt/image"],
 
   css: ["~/assets/css/main.css"],
 
