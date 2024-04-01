40890453080e7b006815dba00ae800242babb881
Matis <matis.baguelin@gmail.com>
2024-04-02 00:08:22 +0200

    feat: simple home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <main>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <main class="flex flex-col justify-center">
+    <div class="flex flex-col space-y-4">
+      <div>
+        <h1 class="text-4xl font-bold mb-2">Hello, je suis Matis Baguelin.</h1>
+        <h2 class="text-xl text-slate-900 font-semibold uppercase">
+          Développeur fullstack.
+        </h2>
+      </div>
+      <p class="text-slate-500">
+        Je suis passionné par les nouvelles technologies, les bonnes pratiques,
+        la rigueur et le développement continu de mes compétences.
+      </p>
+      <NuxtLink to="/projects" class="underline">Voir mes projets</NuxtLink>
+      <div class="flex space-x-4">
+        <NuxtLink
+          v-for="socialLink in socialLinks"
+          :to="socialLink.href"
+          target="_blank"
+        >
+          <component
+            :is="socialLink.icon"
+            size="28px"
+            class-name="fill-gray-600 transition-colors hover:fill-black"
+          />
+        </NuxtLink>
+      </div>
+    </div>
   </main>
 </template>
+
+<script lang="ts" setup>
+import { socialLinks } from "~/utils/links"
+</script>
