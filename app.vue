97e0942ba4ffa831a276a900222bb239ebedd4ba
Matis <matis.baguelin@gmail.com>
2024-04-05 15:41:49 +0200

    feat: add meta tags to head

--- app.vue
+++ app.vue
@@ -3,3 +3,18 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+useHead({
+  title: "Matis Baguelin - Portfolio",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Développeur passionné par les nouvelles technologies et la recherche constante de perfectionnement. Découvrez mes réalisations et compétences en développement web.",
+    },
+  ],
+})
+</script>
