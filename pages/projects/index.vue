9c668272101daf3ed737fabbe3aa3abc9f8d8d86
Matis <matis.baguelin@gmail.com>
2024-04-11 10:37:53 +0200

    style: projects title

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <h1 class="py-4 text-xl font-bold tracking-wide">Mes projets</h1>
+    <h1 class="mb-4 text-4xl font-extrabold tracking-wide">Mes projets</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 md:gap-x-12 md:gap-y-4">
       <ProjectsCard
         v-for="(project, index) in projects"
