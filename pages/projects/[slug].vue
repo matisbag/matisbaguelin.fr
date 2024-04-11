f7de04d1700a489f51f0fb33674750a255989594
Matis <matis.baguelin@gmail.com>
2024-04-11 14:52:10 +0200

    fix: project max width

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,28 +1,30 @@
 <template>
-  <main
-    class="prose prose-img:rounded-lg prose-img:drop-shadow-sm prose-quoteless max-w-none flex flex-col"
-  >
-    <ContentDoc>
-      <template #not-found>
-        <Head>
-          <Title>Projet non trouvé</Title>
-        </Head>
-        <section
-          class="grow flex flex-col md:flex-row-reverse justify-center md:justify-evenly items-center"
-        >
-          <div>
-            <img
-              src="~/assets/img/monamoji-mindblown.png"
-              alt="non trouvé"
-              class="w-80 max-w-full h-auto"
-            />
-          </div>
-          <div>
-            <h1>Projet non trouvé.</h1>
-            <NuxtLink to="/projects"> Voir mes projets </NuxtLink>
-          </div>
-        </section>
-      </template>
-    </ContentDoc>
+  <main>
+    <div
+      class="prose prose-img:rounded-lg prose-img:drop-shadow-sm prose-quoteless max-w-none flex flex-col"
+    >
+      <ContentDoc>
+        <template #not-found>
+          <Head>
+            <Title>Projet non trouvé</Title>
+          </Head>
+          <section
+            class="grow flex flex-col md:flex-row-reverse justify-center md:justify-evenly items-center"
+          >
+            <div>
+              <img
+                src="~/assets/img/monamoji-mindblown.png"
+                alt="non trouvé"
+                class="w-80 max-w-full h-auto"
+              />
+            </div>
+            <div>
+              <h1>Projet non trouvé.</h1>
+              <NuxtLink to="/projects"> Voir mes projets </NuxtLink>
+            </div>
+          </section>
+        </template>
+      </ContentDoc>
+    </div>
   </main>
 </template>
