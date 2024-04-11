951e31c9945b60a1853b49bd4ef32b126464535d
Matis <matis.baguelin@gmail.com>
2024-04-11 14:24:40 +0200

    feat: update homepage design

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,26 @@
 <template>
   <main class="flex flex-col justify-center">
-    <div class="flex flex-col space-y-4">
+    <div class="flex flex-col">
       <div>
-        <h1 class="text-3xl font-bold mb-2">Hello, je suis Matis Baguelin.</h1>
+        <h1 class="text-4xl font-extrabold mb-2">
+          Hello, je suis Matis Baguelin.
+        </h1>
         <h2 class="text-lg text-slate-900 font-semibold uppercase">
           Développeur fullstack.
         </h2>
       </div>
-      <p class="text-slate-500 md:max-w-[720px]">
+      <p class="text-lg text-slate-600 my-9 md:max-w-[720px]">
         Je suis passionné par les nouvelles technologies, les bonnes pratiques,
         la rigueur et le développement continu de mes compétences.
       </p>
-      <NuxtLink to="/projects" class="underline">Voir mes projets</NuxtLink>
+      <NuxtLink to="/projects" class="underline mb-4">
+        Voir mes projets
+      </NuxtLink>
       <div class="flex space-x-4">
         <NuxtLink
           v-for="socialLink in socialLinks"
           :to="socialLink.href"
+          :title="socialLink.title"
           target="_blank"
         >
           <component
