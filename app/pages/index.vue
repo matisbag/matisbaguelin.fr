f2e15d52a9b86f011723e505dd5d43fb46ddf342
Matis <matis.baguelin@gmail.com>
2025-08-12 12:36:26 +0200

    feat: add Panda CSS 🐼

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,12 @@
+<script lang="ts" setup>
+import { css } from 'styled-system/css'
+</script>
+
 <template>
   <main>
     <h1>Hello world!</h1>
+    <div :class="css({ fontSize: '5xl', fontWeight: 'bold' })">
+      Hello 🐼!
+    </div>
   </main>
 </template>
-
-<script lang="ts" setup>
-
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from '@nuxt/kit'
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
+  css: ['~/assets/css/global.css'],
+  alias: {
+    'styled-system': resolve('./styled-system'),
+  },
   compatibilityDate: '2025-07-15',
+
+  postcss: {
+    plugins: {
+      '@pandacss/dev/postcss': {},
+    },
+  },
 })
